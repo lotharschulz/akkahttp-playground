370564277444fa075c4b916ba9d3bb3b5bbd450f
lotharschulz <lothar.schulz@zalando.de>
2016-12-19 17:13:28 +0100

    mem settings adapted

--- docker/akkahttp-playground/run.sh
+++ docker/akkahttp-playground/run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 if [ -z "$JAVA_OPTS" ]; then 
- echo JAVA_OPTS="-Xms64m -Xmx384m"
+ echo JAVA_OPTS="-Xms1024m -Xmx2048m"
 fi
 
 echo "starting with java opts: $JAVA_OPTS"
 
 java $JAVA_OPTS -jar /akkahttp-playground/uberjar.jar
-# java -Xms1024m -Xmx2048m -jar ../../../target/scala-2.11/uberjar.jar
-# java -Xms1024m -Xmx2048m -jar /akkahttp-playground/uberjar.jar
